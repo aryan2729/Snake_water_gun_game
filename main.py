7dd3bda6f3ae19ad1236c2b45364bfad012fdafd
Aryan <143903967+aryan2729@users.noreply.github.com>
2023-12-04 22:50:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 # SNAKE WATER GUN GAME   OR     ROCK PAPER SCISSORS
 
-import random       # it print random what u show this----NEW impp😀
+import random       
 
 def gamewin(comp,you):
-    #If two values are equal , declare a tie!
+    
     if comp == you:
         return None
-    
-    #check for all possibilities when computer chose s
+        
     elif comp == 's':
         if you == 'g':
             return True
         elif you == 'w':
             return False
         
-    #check for all possibilities when computer chose w
     elif comp == 'w':
         if you == 's':
             return True
         elif you == 'g':
             return False
         
-    #check for all possibilities when computer chose g
     elif comp == 'g':
         if you =='w':
             return True
         elif you == 's':
             return False
     
-print("comp turn : Snake(s) Water(w) or Gun(g) ?")
-randomnumber = random.randint(1,3)    # random show 1,2 or 3------- NEW impp😀
+print("comp turn : Snake(s) Water(w) or Gun(g) ? ")
+randomnumber = random.randint(1,3)   
 if randomnumber == 1:
     comp ='s'
 elif randomnumber == 2:
@@ -40,7 +37,7 @@ elif randomnumber ==3:
 you = input("your turn : Snake(s) Water(w) or Gun(g) ?")
 a = gamewin(comp,you)
 
-print(f"Computer chose {comp}")  # it tell what comp chosen and you
+print(f"Computer chose {comp}")  
 print(f"You chose {you}")
 
 if a == None:
